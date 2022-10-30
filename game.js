f268e2a2cebea3fd1d1c78d10e2e7f8a28d26d4d
Erick <erickcampaz@gmail.com>
2022-10-30 17:04:48 -0500

    mapa con Arreglos multidimensionales en JavaScript

--- game.js
+++ game.js
@@ -18,6 +18,7 @@ function setCanvasSize() {
   canvas.setAttribute('height', canvasSize);
   
   elementsSize = canvasSize / 10;
+ 
 
   startGame();
 }
@@ -26,9 +27,21 @@ function startGame() {
   console.log({ canvasSize, elementsSize });
 
   game.font = elementsSize + 'px Verdana';// estos para salga la dimension de la bomba
-  game.textAlign = 'end';
+  game.textAlign = 'end'; // para moverlo su ubicacion
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  const map = maps[0];
+  const mapRows = map.trim().split('\n');
+  const mapRowCols = mapRows.map(row => row.trim().split(''));
+  console.log({map, mapRows, mapRowCols});
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+      game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+    }
   }
 }
+/* este este curso la X se representa --------------,horizontal y la 
+Y
+|
+|
+|
+| vertical */
