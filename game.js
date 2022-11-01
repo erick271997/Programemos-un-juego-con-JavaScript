78cc546e4265cb7b777fb48e53583057ecd7bc19
Erick <erickcampaz@gmail.com>
2022-11-01 18:02:14 -0500

    Cambiando el ciclo for que renderizaba las bomba por otro codigo forEach

--- game.js
+++ game.js
@@ -27,18 +27,34 @@ function startGame() {
   console.log({ canvasSize, elementsSize });
 
   game.font = elementsSize + 'px Verdana';// estos para salga la dimension de la bomba
-  game.textAlign = 'end'; // para moverlo su ubicacion
+  game.textAlign = 'end'; // para moverlo  ubicacion
 
   const map = maps[0];
   const mapRows = map.trim().split('\n');
   const mapRowCols = mapRows.map(row => row.trim().split(''));
   console.log({map, mapRows, mapRowCols});
-  for (let row = 1; row <= 10; row++) {
+
+mapRowCols.forEach((row, rowI) =>{
+    row.forEach((col, colI) =>{
+       const emoji = emojis[col];
+       const posX = elementsSize * (colI +1);
+       const posY = elementsSize * (rowI + 1); 
+       game.fillText(emoji, posX, posY);
+       /* console.log({row, rowI, col,colI}); */
+
+      /*  este codigo se cambio por el ciclo for  */
+    })
+})
+    
+  /* for (let row = 1; row <= 10; row++) {
     for (let col = 1; col <= 10; col++) {
       game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
     }
-  }
-}
+  } */
+} 
+
+
+
 /* este este curso la X se representa --------------,horizontal y la 
 Y
 |
