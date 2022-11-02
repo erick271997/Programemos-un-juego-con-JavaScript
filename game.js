9572f22351a411d1fedcdf6d4ddf761bde6024f4
Erick <erickcampaz@gmail.com>
2022-11-01 19:03:38 -0500

    Creando movimiento de la calabera

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementsSize;
+
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
@@ -18,7 +27,6 @@ function setCanvasSize() {
   canvas.setAttribute('height', canvasSize);
   
   elementsSize = canvasSize / 10;
- 
 
   startGame();
 }
@@ -26,38 +34,60 @@ function setCanvasSize() {
 function startGame() {
   console.log({ canvasSize, elementsSize });
 
-  game.font = elementsSize + 'px Verdana';// estos para salga la dimension de la bomba
-  game.textAlign = 'end'; // para moverlo  ubicacion
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
 
   const map = maps[0];
   const mapRows = map.trim().split('\n');
   const mapRowCols = mapRows.map(row => row.trim().split(''));
   console.log({map, mapRows, mapRowCols});
+  
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
 
-mapRowCols.forEach((row, rowI) =>{
-    row.forEach((col, colI) =>{
-       const emoji = emojis[col];
-       const posX = elementsSize * (colI +1);
-       const posY = elementsSize * (rowI + 1); 
-       game.fillText(emoji, posX, posY);
-       /* console.log({row, rowI, col,colI}); */
+      if (col == 'O') {
+        playerPosition.x = posX;
+        playerPosition.y = posY;
+        console.log({playerPosition});
+      }
+      
+      game.fillText(emoji, posX, posY);
+    });
+  });
 
-      /*  este codigo se cambio por el ciclo for  */
-    })
-})
-    
-  /* for (let row = 1; row <= 10; row++) {
-    for (let col = 1; col <= 10; col++) {
-      game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
-    }
-  } */
-} 
+  movePlayer();
+}
 
+function movePlayer() {
+  game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
+}
 
+window.addEventListener('keydown', moveByKeys);
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
 
-/* este este curso la X se representa --------------,horizontal y la 
-Y
-|
-|
-|
-| vertical */
+function moveByKeys(event) {
+  if (event.key == 'ArrowUp') moveUp();
+  else if (event.key == 'ArrowLeft') moveLeft();
+  else if (event.key == 'ArrowRight') moveRight();
+  else if (event.key == 'ArrowDown') moveDown();
+}
+function moveUp() {
+  console.log('Me quiero mover hacia arriba');
+  playerPosition.y -= elementsSize;
+  movePlayer();
+}
+function moveLeft() {
+  console.log('Me quiero mover hacia izquierda');
+}
+function moveRight() {
+  console.log('Me quiero mover hacia derecha');
+}
+function moveDown() {
+  console.log('Me quiero mover hacia abajo');
+}
